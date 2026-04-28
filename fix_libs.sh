23cc999130bf8d53a758e7d875c63ae6f4fb54c6
AngelBePro <user@holybluds.xyz>
2026-04-28 17:14:46 +0300

    Another veeery small touchup
    JIC use env variable instead of ~

--- fix_libs.sh
+++ fix_libs.sh
@@ -18,16 +18,16 @@ done
 
 # _______ in this case is the clover app for the tower puzzle, but as to not spoil I leave it mysterious with the original name of the app
 echo "Fixing _______ app libs"
-if [ -d ~/.local/share/Oneshot ]; then
+if [ -d $HOME/.local/share/Oneshot ]; then
 	echo "Game data folder already exists."
 else
-	mkdir ~/.local/share/Oneshot
+	mkdir $HOME/.local/share/Oneshot
 fi
 
 # For some reason doesn't work with symlinks so we use hard links instead
-if [ -f ~/.local/share/Oneshot/base_library.zip ]; then
+if [ -f $HOME/.local/share/Oneshot/base_library.zip ]; then
 	echo "Game data folder already fixed"
 else
-ln ./base_library.zip ./backuplibs/* ~/.local/share/Oneshot
-ln -s $PWD/images $PWD/PyQt5 ~/.local/share/Oneshot
+	ln ./base_library.zip ./backuplibs/* $HOME/.local/share/Oneshot
+	ln -s $PWD/images $PWD/PyQt5 $HOME/.local/share/Oneshot
 fi
